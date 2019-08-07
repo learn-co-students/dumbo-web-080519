b945915c45614530c9445267502392c271e44ac6
Eric Kim <hyeokjungkim@gmail.com>
2019-08-07 11:12:57 -0400

    adds lecture

--- 03-intro-oo/clown.rb
+++ 03-intro-oo/clown.rb
@@ -2,15 +2,54 @@ require 'pry'
 
 clown_joke = "Why don't sharks eat clowns? They taste funny."
 clown_names = [ "Bluster", "Flaky", "Raspy", "Floppy", "Krusty", "Baby" ]
-#
-# clown1 = { name: "Bluster", age: 40, skill: "juggling", fear: "red noses" }
-# clown2 = { name: "Flaky", age: 32, skills: "pie throwing", fears: "children" }
-# clown3 = { name: "Raspy", age: 24, skill: "haunting your dreams", fear: "other clowns" }
-# clown4 = { name: "Floppy", clown_age: 76, skill: "falling down", fear: "stairs" }
-# clown5 = { name: "Krusty", age: 39, skill: "giving a creepy smile", fear: "parties" }
-# clown6 = { name: "Baby", age: 19, skills: "eating a lot of food", fear: "birds" }
-#
-# clowns = [ clown1, clown2, clown3, clown4, clown5, clown6 ]
+
+class Clown
+  # Reader/Getter
+  attr_reader :hair_color, :fear
+
+  # Writer/Setter
+  # attr_writer :hair_color
+
+  # Both a reader and a writer
+  attr_accessor :name, :age, :skill
+
+  def initialize(name, age, skill, fear)
+    @name = name
+    @age = age
+    @skill = skill
+    @fear = fear
+    @hair_color = "red"
+  end
+
+  # Behavior
+  def dance
+    puts "#{self.name} is dancing, la la la."
+  end
+
+  def cure
+    if @age > 30
+      @fear = "Nothing"
+    end
+  end
+
+  def fear_reassignment=(new_fear)
+    if @age > 30
+      @fear = new_fear
+    end
+  end
+
+end
+
+
+clown1 = Clown.new("Bluster", 40, "juggling", "red noses")
+clown2 = Clown.new("Flaky", 32, "pie throwing", "children")
+clown3 = Clown.new("Raspy", 24, "haunting your dreams","other clowns")
+clown4 = Clown.new("Floppy", 76, "falling down","stairs")
+clown5 = Clown.new("Krusty", 39, "giving a creepy smile","parties")
+clown6 = Clown.new("Baby", 19, "eating a lot of food","birds")
+
+
+
 
 
 binding.pry
