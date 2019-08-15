ce6847d9e00ee021fda3d5ac16c5df9d4a947b29
Eric Kim <hyeokjungkim@gmail.com>
2019-08-15 11:34:06 -0400

    adds lecture

--- 08-intro-to-activerecord/bin/mass_assignment_notes.rb
+++ 08-intro-to-activerecord/bin/mass_assignment_notes.rb
@@ -1,1 +1,19 @@
 require_relative "../config/environment"
+
+class Clown
+
+  attr_accessor :name, :age, :fears, :skill
+
+  def initialize(argument_hash)
+    argument_hash.each do |attribute, value|
+      # self.name = "Krusty"
+      self.send("#{attribute}=", value)
+    end
+  end
+
+end
+
+krusty = Clown.new({name: "Krusty", fears: "Children", skill: "Juggling Chainsaws"})
+
+binding.pry
+0
