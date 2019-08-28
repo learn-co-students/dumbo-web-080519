aa2c63f4163c6323832071aaa2aa9fd07e9ce66e
Eric Kim <hyeokjungkim@gmail.com>
2019-08-28 14:02:05 -0400

    done

--- 13-Sinatra-CR/app/controllers/application_controller.rb
+++ 13-Sinatra-CR/app/controllers/application_controller.rb
@@ -23,12 +23,30 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  # NEW action
+  get '/students/new' do
+    erb :new
+  end
+
+  # CREATE action
+  post '/students' do
+    # old params => {name: "Mad Dog", age: 2}
+    # new params => {name: "Mad Dog", age: 2, submitty: "Create a new student"}
+
+    # ideal params => {student: {name: "Mad Dog", age: 2}, submitty: "Create a new student"}
+
+    # binding.pry
+    @student = Student.create(params[:student])
+    redirect "/students/#{@student.id}"
+  end
+
   # SHOW action
   get '/students/:id' do
     # binding.pry
     @student = Student.find(params[:id])
     erb :show
   end
+
 
 
 
