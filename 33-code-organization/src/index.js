29f33c2886f60fcd35635e72685c285f959ffe91
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-09-30 09:36:58 -0400

    toy tale init

---
+++ 33-code-organization/src/index.js
@@ -0,0 +1,18 @@
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+let addToy = false
+
+// YOUR CODE HERE
+
+addBtn.addEventListener('click', () => {
+  // hide & seek with the form
+  addToy = !addToy
+  if (addToy) {
+    toyForm.style.display = 'block'
+  } else {
+    toyForm.style.display = 'none'
+  }
+})
+
+
+// OR HERE!
