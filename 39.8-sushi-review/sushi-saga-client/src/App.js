aa738259cae031b22f840614d2740096361b3ea4
sylwiavargas <sylwia.vargas@gmail.com>
2019-10-14 19:39:41 -0400

    refactored code

--- 39.8-sushi-review/sushi-saga-client/src/App.js
+++ 39.8-sushi-review/sushi-saga-client/src/App.js
@@ -17,16 +17,14 @@ class App extends Component {
   eatSushi = (sushi) => {
     let currentBudget = this.state.budget - sushi.price
     this.state.budget >= 0 ? 
-      this.setState(prevState => {
-        return {
-          eaten: [...prevState.eaten, sushi], 
-          budget: currentBudget
-        }
+      this.setState({
+        eaten: [...this.state.eaten, sushi], 
+        budget: currentBudget
       })
     : alert("no dollarz")
   }
 
-  fetchSushi = () => {
+  componentDidMount () {
     fetch(API)
     .then(res => res.json())
     .then(data => {
@@ -36,13 +34,31 @@ class App extends Component {
     })
   }
 
-  componentDidMount () {
-    this.fetchSushi()
-  }
-
   fourSushi = () => {
     const newNumber = this.state.currentIndex + 4
     return this.state.sushis.slice(this.state.currentIndex, newNumber)
+  }
+
+  // moreSushi = () => {    
+  //   let newNumber;
+
+  //   if (this.state.currentIndex >= 98) {
+  //     newNumber = 0 
+  //     return this.setState({
+  //       currentIndex: newNumber
+  //     })
+  //    } else {
+  //     newNumber = this.state.currentIndex + 4
+  //     return this.setState({
+  //       currentIndex: newNumber
+  //     })
+  //    } 
+  // }
+
+  moreSushi = () => {    
+    let newNumber;
+    this.state.currentIndex >= 96 ?  newNumber = 0 : newNumber = this.state.currentIndex + 4
+    return newNumber
   }
 
   updateNumber = (newNumber) => {
@@ -51,19 +67,22 @@ class App extends Component {
     })
   }
 
-  moreSushi = () => {    
-    let newNumber;
-    this.state.currentIndex >= 96 ?  newNumber = 0 : newNumber = this.state.currentIndex + 4
-    return newNumber
-  }
-
   render() {
+    // console.log(this.state.currentIndex)
+    // console.log("here are your eaten sush", this.state.eaten)
+    // console.log("index", this.state.currentIndex)
+    // console.log("fourSushi", this.fourSushi())
+    // console.log("newNumber", this.moreSushi())
+    // console.log("currentIndex", this.state.currentIndex)
+    // console.log("dollarz", this.state.budget)
+
     return (
       <div className="app">
         <SushiContainer 
           sushis={this.fourSushi()} 
           eaten={this.state.eaten}
           eatSushi={this.eatSushi}
+          // moreSushi={this.moreSushi}
           moreSushi={() => this.updateNumber(this.moreSushi())}
           budget= {this.state.budget}/>
         <Table 
