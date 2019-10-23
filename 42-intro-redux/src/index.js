39db4d547945d767efe29f71081927dfd5da87af
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-10-23 14:30:54 -0400

    redux

---
+++ 42-intro-redux/src/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux'
+// import reducer from './reducer'
+
+const initialState = {
+
+}
+
+// const reducer = (state, action) => {
+
+// }
+
+// const store = createStore(reducer, initialState)
+
+// console.log(store)
+// console.log(store.getState())
+
+
